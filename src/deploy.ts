3e6ddd71fb9a121353694e1e89a7077eb10c1044
Cybore8 <flameodin@gmail.com>
2023-06-15 23:13:51 -0700

    Update deploy.ts

--- src/deploy.ts
+++ src/deploy.ts
@@ -2568,7 +2568,8 @@ export default async function deploy(
 	/*                                         Generate RPKGs                                         */
 	/* ---------------------------------------------------------------------------------------------- */
 	await logger.info("Generating RPKGs")
-
+	
+	const data = fs.readFileSync('./param.txt');
 	const sentryRPKGGenerationTransaction = sentryTransaction.startChild({
 		op: "stage",
 		description: "RPKG generation"
@@ -2581,8 +2582,8 @@ export default async function deploy(
 			fs.copyFileSync(
 				path.join(process.cwd(), "staging", `${stagingChunkFolder}.rpkg`),
 				config.outputToSeparateDirectory
-					? path.join(process.cwd(), "Output", allRPKGTypes[stagingChunkFolder] === "base" ? `${stagingChunkFolder}.rpkg` : `${stagingChunkFolder}patch305.rpkg`)
-					: path.join(config.runtimePath, allRPKGTypes[stagingChunkFolder] === "base" ? `${stagingChunkFolder}.rpkg` : `${stagingChunkFolder}patch305.rpkg`)
+					? path.join(process.cwd(), "Output", allRPKGTypes[stagingChunkFolder] === "base" ? `${stagingChunkFolder}.rpkg` : `${stagingChunkFolder}patch`${data}`.rpkg`)
+					: path.join(config.runtimePath, allRPKGTypes[stagingChunkFolder] === "base" ? `${stagingChunkFolder}.rpkg` : `${stagingChunkFolder}patch`${data}`.rpkg`)
 			)
 		} catch {
 			await logger.error("Couldn't copy the RPKG files! Make sure the game isn't running when you deploy your mods.")
