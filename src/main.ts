52dd8d38ef4174712cfea07cd5a8258adb395b38
Cybore8 <84304694+Cybore8@users.noreply.github.com>
2023-08-17 07:41:09 -0700

    No hashcheck main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,22 +16,6 @@ import { xxhash3 } from "hash-wasm"
 
 require("clarify")
 
-const gameHashes = {
-	f9b0347f278b533ace9a744b5b5353f9: Platform.epic, // base game
-	c811d53d32fe1dc4e44546df2ab45638: Platform.epic, // ansel unlock
-	"1e245f331499dfd25206276e58f0f81f": Platform.epic, // ansel no collision
-	f0c6daf3f4610965c45aba2c9edda254: Platform.epic, // ansel unlock + no collision
-	"81f5ec2450d4369583d28495445311f6": Platform.steam, // base game
-	d5a26538d4500aac83a4767293f10921: Platform.steam, // ansel unlock
-	"7be8ce65668d007d4d1f4d0ab80153e3": Platform.steam, // ansel no collision
-	c5dcb9edd2c348ca21bcd490a867f717: Platform.steam, // ansel unlock + no collision
-
-	// Gamepass/store protects the EXE from reading so we can't hash it, instead we hash the game config
-	"05a3152429d216c83a612dfbc25d3e63": Platform.microsoft
-} as {
-	[k: string]: Platform
-}
-
 if (!core.config.reportErrors) {
 	process.on("uncaughtException", (err, origin) => {
 		void (async () => {
@@ -78,9 +62,7 @@ if (fs.existsSync(path.join(core.config.retailPath, "Runtime", "chunk0.rpkg"))) 
 	}
 }
 
-core.config.platform = fs.existsSync(path.join(core.config.retailPath, "Runtime", "chunk0.rpkg"))
-	? gameHashes[md5File.sync(path.join(core.config.retailPath, "..", "MicrosoftGame.Config"))]
-	: gameHashes[md5File.sync(path.join(core.config.runtimePath, "..", "Retail", "HITMAN3.exe"))] // Platform detection
+core.config.platform = core.config.platform ?? Platform.epic;
 
 let sentryTransaction = {
 	startChild(...args) {
